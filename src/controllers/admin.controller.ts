8f75b8107b1795e093e79066ff857d738b2b9546
sahnoun122 <khadijasahnoun70@gmail.com>
2025-10-29 11:21:28 +0100

    feat: ajouter la functions pour accepter des annoces

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -9,3 +9,15 @@ export const getPendingProperties = async (req: Request, res: Response) => {
 		res.status(500).json({ message: `erreur lors de la recuperations des annonces ` });
 	}
 };
+
+export const approveProperty = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const property = await Property.findByIdAndUpdate(id, { status: 'approved' }, { new: true });
+		if (!property) return res.status(404).json({ message: 'annonce introuvable' });
+		res.status(200).json({ message: 'annonce accepter avec succes ', property });
+	} catch (error) {
+		res.status(500).json({ message: "errore lors l'acceptations " });
+	}
+};
